e2d53c49aa557ecfec953276f65b2bbfb1ec4b18
fikalisAP <ivan.sherbetov@yandex.ru>
2022-02-09 14:00:40 +0500

    burger and gallery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-$('.reviews__slider').slick({
-    dots: true,
-    arrows: false
+
+$(function () {
+    $('.menu__btn').on('click', function () {
+        $('.menu__items').toggleClass('active');
+    });
+    $('.reviews__slider').slick({
+        dots: true,
+        arrows: false
+    });
+
+    $('[data-fancybox]').fancybox({
+        protect: true,
+        buttons: [
+            'zoom',
+            'thumbs',
+            'close'
+        ]
+    });
+
 });
+
+
+
